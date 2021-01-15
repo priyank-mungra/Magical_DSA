6753107fad1b3d62ded7d4736d46e8d99cf41c81
priyank-mungra <priyankmungra289@gmail.com>
2021-01-15 17:25:46 +0530

    add compareAnimation() , BUG FIXES, refactoring
    bug fix of lenght - 1 in BubbleSort
    optimised BubbleSort
    refactor: place "turn Green Again code" from BubbleSort to compareAnimation

--- Mark-1/js/BubbleSort.js
+++ Mark-1/js/BubbleSort.js
@@ -3,17 +3,19 @@ var noInterruption = true;
 
 async function ascendingBubbleSort() {
     if (isContinuous) {
-        alert("WAIT: Descending in process");
+        alert("WAIT: Sorting in process");
         return -1;
     }
+    isContinuous = true;
+
     d3.selectAll('rect')
         .style('fill', 'green');
-    isContinuous = true;
     let len = currentValue.length;
     let swapped;
+    let j = 0;
     do {
         swapped = false;
-        for (let i = 0; i < len && noInterruption; i++) {
+        for (let i = 0; i+j < len-1 && noInterruption; i++) {
             if (currentValue[i] > currentValue[i + 1]) {
 
                 let temp = currentValue[i];
@@ -22,11 +24,15 @@ async function ascendingBubbleSort() {
                 swapped = true;
                 // call function here for swap animation...
                 await swapAnimation(currentValue[i], currentValue[i + 1]);
-                d3.selectAll('rect')
-                    .style('fill', 'green');
+                // d3.selectAll('rect')
+                //     .style('fill', 'green');
                 // console.log(currentValue);
             }
+            else{
+                await compareAnimation(currentValue[i], currentValue[i + 1]);
+            }
         }
+        j++;
     } while (swapped && noInterruption);
     // return console.log(currentValue);
     if (noInterruption) {
@@ -42,17 +48,18 @@ async function ascendingBubbleSort() {
 
 async function descendingBubbleSort() {
     if (isContinuous) {
-        alert("WAIT: Ascending in process");
+        alert("WAIT: Sorting in process");
         return -1;
     }
+    isContinuous = true;
     d3.selectAll('rect')
         .style('fill', 'green');
-    isContinuous = true;
     let len = currentValue.length;
     let swapped;
+    let j = 0;
     do {
         swapped = false;
-        for (let i = 0; i < len && noInterruption; i++) {
+        for (let i = 0; i+j < len-1 && noInterruption; i++) {
             if (currentValue[i] < currentValue[i + 1]) {
 
                 let temp = currentValue[i];
@@ -61,11 +68,15 @@ async function descendingBubbleSort() {
                 swapped = true;
                 // call function here for swap animation...
                 await swapAnimation(currentValue[i], currentValue[i + 1]);
-                d3.selectAll('rect')
-                    .style('fill', 'green');
+                // d3.selectAll('rect')
+                //     .style('fill', 'green');
                 // console.log(currentValue);
             }
+            else{
+                await compareAnimation(currentValue[i], currentValue[i + 1]);
+            }
         }
+        j++;
     } while (swapped && noInterruption);
     // return console.log(currentValue);
     if (noInterruption) {
@@ -77,42 +88,4 @@ async function descendingBubbleSort() {
     }
     isContinuous = false;
     noInterruption = true;
-}
-
-function swapAnimation(d, d1) {
-    var rect1 = `#rect${d}`,
-        rect2 = `#rect${d1}`
-    var rect_x_2 = d3.select(rect2).attr("x")
-    var rect_x_1 = d3.select(rect1).attr("x")
-    var text1 = `#text${d}`,
-        text2 = `#text${d1}`
-    var text_x_2 = d3.select(text2).attr("x")
-    var text_x_1 = d3.select(text1).attr("x")
-    return Promise.all([
-        d3.select(text1)
-        .transition()
-        .duration(duration)
-        .attr("x", text_x_2)
-        .end(),
-
-        d3.select(text2)
-        .transition()
-        .duration(duration)
-        .attr("x", text_x_1)
-        .end(),
-
-        d3.select(rect1)
-        .transition()
-        .duration(duration)
-        .style('fill', 'orange')
-        .attr("x", rect_x_2)
-        .end(),
-
-        d3.select(rect2)
-        .transition()
-        .duration(duration)
-        .style('fill', 'orange')
-        .attr("x", rect_x_1)
-        .end()
-    ])
 }
--- Mark-1/js/Simulation.js
+++ Mark-1/js/Simulation.js
@@ -9,3 +9,83 @@ function resetInput() {
     createChart();
     // console.log("currentValue", currentValue);
 }
+
+function swapAnimation(d, d1) {
+    var rect1 = `#rect${d}`,
+        rect2 = `#rect${d1}`
+    var rect_x_2 = d3.select(rect2).attr("x")
+    var rect_x_1 = d3.select(rect1).attr("x")
+    var text1 = `#text${d}`,
+        text2 = `#text${d1}`
+    var text_x_2 = d3.select(text2).attr("x")
+    var text_x_1 = d3.select(text1).attr("x")
+    return Promise.all([
+        d3.select(text1)
+        .transition()
+        .duration(duration)
+        .attr("x", text_x_2)
+        .end(),
+
+        d3.select(text2)
+        .transition()
+        .duration(duration)
+        .attr("x", text_x_1)
+        .end(),
+
+        d3.select(rect1)
+        .transition()
+        .duration(duration)
+        .style('fill', 'orange')
+        .attr("x", rect_x_2)
+        .end(),
+
+        d3.select(rect2)
+        .transition()
+        .duration(duration)
+        .style('fill', 'orange')
+        .attr("x", rect_x_1)
+        .end(),
+
+        d3.selectAll('rect').style('fill', 'green')
+    ])
+}
+
+function compareAnimation(d, d1) {
+    var rect1 = `#rect${d}`,
+        rect2 = `#rect${d1}`
+    var rect_x_2 = d3.select(rect2).attr("x")
+    var rect_x_1 = d3.select(rect1).attr("x")
+    // var text1 = `#text${d}`,
+    //     text2 = `#text${d1}`
+    // var text_x_2 = d3.select(text2).attr("x")
+    // var text_x_1 = d3.select(text1).attr("x")
+    return Promise.all([
+        // d3.select(text1)
+        // .transition()
+        // .duration(duration)
+        // .attr("x", text_x_2)
+        // .end(),
+
+        // d3.select(text2)
+        // .transition()
+        // .duration(duration)
+        // .attr("x", text_x_1)
+        // .end(),
+
+        d3.select(rect1)
+        .transition()
+        .duration(duration)
+        .style('fill', 'orange')
+        .attr("x", rect_x_1)
+        .end(),
+
+        d3.select(rect2)
+        .transition()
+        .duration(duration)
+        .style('fill', 'orange')
+        .attr("x", rect_x_2)
+        .end(),
+
+        d3.selectAll('rect').style('fill', 'green')
+    ])
+}
