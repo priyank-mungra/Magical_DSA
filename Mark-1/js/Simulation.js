4a875b139e8e71eefcfdd7672b86feab6134a9f2
yash-a-18 <yashamethiya2001@gmail.com>
2021-01-16 11:20:54 +0530

    Add login link and duration

--- Mark-1/js/Simulation.js
+++ Mark-1/js/Simulation.js
@@ -55,23 +55,7 @@ function compareAnimation(d, d1) {
         rect2 = `#rect${d1}`
     var rect_x_2 = d3.select(rect2).attr("x")
     var rect_x_1 = d3.select(rect1).attr("x")
-    // var text1 = `#text${d}`,
-    //     text2 = `#text${d1}`
-    // var text_x_2 = d3.select(text2).attr("x")
-    // var text_x_1 = d3.select(text1).attr("x")
     return Promise.all([
-        // d3.select(text1)
-        // .transition()
-        // .duration(duration)
-        // .attr("x", text_x_2)
-        // .end(),
-
-        // d3.select(text2)
-        // .transition()
-        // .duration(duration)
-        // .attr("x", text_x_1)
-        // .end(),
-
         d3.select(rect1)
         .transition()
         .duration(duration)
@@ -86,6 +70,7 @@ function compareAnimation(d, d1) {
         .attr("x", rect_x_2)
         .end(),
 
-        d3.selectAll('rect').style('fill', 'green')
+        d3.selectAll('rect')
+        .style('fill', 'green')
     ])
 }
